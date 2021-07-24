7499263128bd2fe733f3db678d3d00c6972cadb0
Tanner W. York <tyork@zeta-apps.com>
2021-07-24 18:04:31 -0500

    Add tests /routes for user report actions

--- controllers/reports.js
+++ controllers/reports.js
@@ -1,90 +1,134 @@
-const Report = require('../models/Report.js');
-const User = require('../models/User.js');
+const Report = require("../models/Report.js");
+const User = require("../models/User.js");
 
-module.exports = function(app) {
-
-  app.get('/reports', (req, res) => {
-    Report.find({})
-      .then(reports => {
-        res.send(reports);
-      })
-      .catch(err => {
-        res.status(400).send({error: err.message});
-      });
-  })
-
-  app.post('/report/new', (req, res) => {
-    if (req.user) {
-      const report  = new Report(req.body);
-      report.author = req.user._id;
-      report
-        .save()
-        .then(report => {
-          return User.findById(req.user._id);
-        })
-        .then(user => {
-          user.reports.unshift(report);
-          user.save();
-          res.send(report);
-        })
-        .catch(err => {
-          console.log(err.message);
-          res.send({ error: err.message});
-        });
-    } else {
-      return res.status(401);
-    }
-  });
-  
-  app.get("/reports/:id", (req, res) => {
-    Report.findById(req.params.id).lean()
-      .populate('author')
-      .then((report) => {
-        res.send(report);
-      })
-      .catch(err => {
-        console.log(err.message);
-        res.send({ error: err.message});
-      });
-  });
-
-  app.put("/reports/:id", (req, res) => {
-    Report.findById(req.params.id).lean()
-      .populate('author')
-      .then((report) => {
-        if (req.user != null && report.author._id == req.user._id) {
-          Report.findByIdAndUpdate(req.params.id, req.body, {new: true})
-            .then(report => {
-              console.log('===========')
-              console.log(report)
-              res.send(report);
-            })
-            .catch(err => {
-              console.log(err)
-              res.status(400).send({error: err.message});
-            });
-        } else {
-          res.status(400).send({error: "You can only update your reports."})
+module.exports = function (app) {
+    app.get("/reports", async (req, res) => {
+        // Veirfy user is logged in
+        const user = req.user;
+        if (!user) {
+            return res.status(401).send({ message: "You need to be logged in to access this route." });
         }
-      })
-      .catch(err => {
-        console.log(err.message);
-        res.status(400).send({error: err.message});
-      });
-  });
 
-  app.delete("/reports/:id", (req, res) => {
-    Report.findById(req.params.id)
-      .then(report => {
-        if (report.author._id == req.user._id) {
-          report.delete().then(() => {
-            res.send({ success: `Successfully deleted the report ${req.params.id}`});
-          }).catch(err => {
-            res.send({ error: err.message });
-          });
-        } else {
-          res.status(401).send({ error: 'You can only delete your own reaports'});
+        // Get all reports
+        const reports = await Report.find({});
+
+        // Send successful response
+        return res.send({ reports });
+    });
+
+    app.get("/my/reports", async (req, res) => {
+        // Veirfy user is logged in
+        const user = req.user;
+        if (!user) {
+            return res.status(401).send({ message: "You need to be logged in to access this route." });
         }
-      });
-  });
+
+        // Get all of the user's reports
+        const reports = await Report.find({ author: user._id });
+
+        // Send successful response
+        return res.send({ reports });
+    });
+
+    app.post("/report/new", async (req, res) => {
+        // Veirfy user is logged in
+        const user = req.user;
+        if (!user) {
+            return res.status(401).send({ message: "You need to be logged in to access this route." });
+        }
+
+        // Create new report and set report author to current user
+        const report = new Report({ ...req.body, author: user._id });
+
+        // Save report to database
+        await report.save();
+
+        // Send successful response
+        return res.send({ message: "Successfully created new report." });
+    });
+
+    app.get("/reports/:reportId", async (req, res) => {
+        // Get url parameters
+        const { reportId } = req.params;
+
+        // Veirfy user is logged in
+        const user = req.user;
+        if (!user) {
+            return res.status(401).send({ message: "You need to be logged in to access this route." });
+        }
+
+        // Verify requested report exists
+        const report = await Report.findById(reportId);
+        if (!report) {
+            return res.status(400).send({ message: "Could not find requested report." });
+        }
+
+        // Return successful response
+        return res.send({ report });
+    });
+
+    app.put("/reports/:reportId", async (req, res) => {
+        // Get url parameters
+        const { reportId } = req.params;
+
+        // Veirfy user is logged in
+        const user = req.user;
+        if (!user) {
+            return res.status(401).send({ message: "You need to be logged in to access this route." });
+        }
+
+        // Verify requested report exists
+        const report = await Report.findById(reportId);
+        if (!report) {
+            return res.status(400).send({ message: "Could not find requested report." });
+        }
+
+        // Verify current user is the reports author
+        if (String(report.author) != String(user._id)) {
+            return res.status(401).send({ message: "You do not have access to update this report." });
+        }
+
+        // Update report
+        const updatedReport = await Report.findByIdAndUpdate(report._id, req.body, { new: true });
+
+        // Verify update was successful
+        if (!updatedReport) {
+            return res.status(400).send({ message: "An error occured while updating report." });
+        }
+
+        // Return successful response
+        return res.send({ report: updatedReport });
+    });
+
+    app.delete("/reports/:reportId", async (req, res) => {
+        // Get url parameters
+        const { reportId } = req.params;
+
+        // Veirfy user is logged in
+        const user = req.user;
+        if (!user) {
+            return res.status(401).send({ message: "You need to be logged in to access this route." });
+        }
+
+        // Verify requested report exists
+        const report = await Report.findById(reportId);
+        if (!report) {
+            return res.status(400).send({ message: "Could not find requested report." });
+        }
+
+        // Verify current user is the reports author
+        if (String(report.author) != String(user._id)) {
+            return res.status(401).send({ message: "You do not have access to update this report." });
+        }
+
+        // Delete Report
+        try {
+            await Report.deleteOne({ _id: reportId });
+        } catch (err) {
+            return res.status(400).send({ message: err.message });
+        }
+
+        // Send a successful response
+        return res.send({ message: "Successfully deleted report." });
+    });
 };
--- test/reports.js
+++ test/reports.js
@@ -4,7 +4,7 @@ const chaiHttp = require("chai-http");
 
 const expect = chai.expect;
 const agent = chai.request.agent(app);
-const adminAgent = chai.request.agent(app);
+const otherAgent = chai.request.agent(app);
 
 const Report = require("../models/Report.js");
 const User = require("../models/User.js");
@@ -16,9 +16,9 @@ chai.use(chaiHttp);
 describe("Reports", function () {
     // Post that we'll use for testing purposes
     var authorityId = "5ddf784f9dbb9f1530b96033";
-    var adminId = "5ddf76af01c5992ec4fa1c8c";
+    var otherId = "6ddf86af01c5992ec4fa1c8c";
     var userId = "5ddf76af01c5992ec4fa1c9c";
-    var reportId = "5ddf784f9dbb9f1530b96022";
+    var reportId = "6ddf784f9dbb9f1530b96022";
 
     before(function (done) {
         const authority = new Authority({
@@ -32,18 +32,16 @@ describe("Reports", function () {
     });
 
     before(function (done) {
-        adminAgent
+        otherAgent
             .post("/sign-up")
             .set({ "content-type": "application/json" })
             .send({
-                _id: adminId,
-                username: "testadmin",
-                email: "admin@email.com",
+                _id: otherId,
+                username: "testother",
+                email: "other@email.com",
                 password: "password",
-                authority: authorityId,
-                access_level: "admin",
             })
-            .end((err, res) => {
+            .end(function (err, res) {
                 expect(res.status).to.equal(200);
                 done();
             });
@@ -65,173 +63,112 @@ describe("Reports", function () {
             });
     });
 
-    before(function (done) {
+    before(async function () {
         const report = new Report({
             _id: reportId,
             title: "report",
             details: "report details",
             author: userId,
+            authority: authorityId,
         });
-        report.save();
-        done();
+        await report.save();
+        return;
     });
 
     it("should get all reports at GET /reports", function (done) {
+        agent.get("/reports").end((err, res) => {
+            console.log(res.body);
+            res.should.have.status(200);
+            expect(res.body).to.have.property("reports");
+            expect(res.body.reports).to.be.an("array");
+            done();
+        });
+    });
+
+    it("should create with valid attributes at POST /report/new", async function () {
+        const initialDocCount = await Report.estimatedDocumentCount();
+
         agent
-            .get("/reports")
-            .then(function (res) {
-                res.should.have.status(200);
-                expect(res.body).to.be.an("array");
-                done();
-            })
-            .catch((err) => {
-                done(err);
+            .post("/report/new")
+            .set("content-type", "application/json")
+            .send({ title: "report", details: "detials", authority: authorityId })
+            .end(async (err, res) => {
+                console.log(res.body);
+                expect(res).to.have.status(200);
+                const newDocCount = await Report.estimatedDocumentCount();
+                expect(newDocCount).to.be.equal(initialDocCount + 1);
+                return;
             });
     });
 
-    it("should get all authority reports at GET /authority/reports", function (done) {
-        adminAgent
-            .get("/authority/reports")
-            .then(function (res) {
-                res.should.have.status(200);
-                expect(res.body).to.be.an("array");
-                done();
-            })
-            .catch((err) => {
-                done(err);
-            });
+    it("should get all user reports at GET /my/reports", function (done) {
+        agent.get("/my/reports").end((err, res) => {
+            console.log(res.body);
+            res.should.have.status(200);
+            expect(res.body).to.have.property("reports");
+            expect(res.body.reports).to.be.an("array");
+            done();
+        });
     });
 
-    it("should not get all reports at GET /authority/reports if no authority", function (done) {
+    it("should get specifc report at GET /reports/:reportId", function (done) {
+        agent.get(`/reports/${reportId}`).end((err, res) => {
+            console.log(res.body);
+            res.should.have.status(200);
+            expect(res.body).to.be.an("object");
+            expect(res.body).to.have.property("report");
+            expect(res.body.report).to.be.an("object");
+            done();
+        });
+    });
+
+    it("should update specifc report at PUT /reports/:reportId", function (done) {
         agent
-            .get("/authority/reports")
-            .then(function (res) {
-                res.should.have.status(401);
-                expect(res.body).to.be("object");
-                expect(res.body).to.haveOwnProperty("error");
-                done();
-            })
-            .catch((err) => {
-                done(err);
-            });
-    });
-
-    it("should create with valid attributes at POST /report/new", function (done) {
-        Report.estimatedDocumentCount()
-            .then(function (initialDocCount) {
-                agent
-                    .post("/report/new")
-                    .set("content-type", "application/json")
-                    .send({ title: "report", details: "detials", authority: authorityId })
-                    .then(function (res) {
-                        Report.estimatedDocumentCount()
-                            .then(function (newDocCount) {
-                                expect(res).to.have.status(200);
-                                expect(newDocCount).to.be.equal(initialDocCount + 1);
-                                done();
-                            })
-                            .catch(function (err) {
-                                done(err);
-                            });
-                    })
-                    .catch(function (err) {
-                        done(err);
-                    });
-            })
-            .catch(function (err) {
-                done(err);
-            });
-    });
-
-    it("should get specifc report at GET /authority/reports/reportId:", function (done) {
-        adminAgent
-            .get(`/authority/reports/${reportId}`)
-            .then(function (res) {
-                res.should.have.status(200);
-                expect(res.body).to.be.an("object");
-                done();
-            })
-            .catch((err) => {
-                done(err);
-            });
-    });
-
-    it("should not get specifc report at GET /authority/reports/reportId if no authority", function (done) {
-        agent
-            .get(`/authority/reports/${reportId}`)
-            .then(function (res) {
-                res.should.have.status(401);
-                expect(res.body).to.be.an("object");
-                expect(res.body).to.have.property("error");
-                done();
-            })
-            .catch((err) => {
-                done(err);
-            });
-    });
-
-    it("should update specifc report at PUT /authority/reports/reportId", function (done) {
-        adminAgent
-            .put(`/authority/reports/${reportId}`)
+            .put(`/reports/${reportId}`)
             .set("content-type", "application/json")
             .send({ details: "new details" })
-            .then(function (res) {
+            .end((err, res) => {
+                console.log(res.body);
                 res.should.have.status(200);
                 expect(res.body).to.be.an("object");
-                expect(res.body.details).to.equal("new details");
+                expect(res.body).to.have.property("report");
+                expect(res.body.report.details).to.equal("new details");
                 done();
-            })
-            .catch((err) => {
-                done(err);
             });
     });
 
-    it("should not update specifc report at PUT /authority/reports/reportId if not authority admin", function (done) {
-        agent
-            .put(`/authority/testauthority/reports/${reportId}`)
+    it("should not update specifc report at PUT /reports/:reportId if not report author", function (done) {
+        otherAgent
+            .put(`/reports/${reportId}`)
             .set("content-type", "application/json")
             .send({ details: "new details" })
-            .then(function (res) {
+            .end((err, res) => {
+                console.log(res.body);
                 res.should.have.status(401);
                 expect(res.body).to.be.an("object");
-                expect(res.body).to.property("error");
+                expect(res.body).to.property("message");
                 done();
-            })
-            .catch((err) => {
-                done(err);
             });
     });
 
-    it("should delete report at DELET /authority/reports/reportId", function (done) {
-        adminAgent
-            .delete(`/authority/reports/${reportId}`)
-            .set("content-type", "application/json")
-            .send()
-            .then(function (res) {
-                res.should.have.status(200);
-                expect(res.body).to.be.an("object");
-                expect(res.body).to.have.property("success");
-                done();
-            })
-            .catch((err) => {
-                done(err);
-            });
+    it("should not delete report at DELETE /reports/:reportId is not report author", function (done) {
+        otherAgent.delete(`/reports/${reportId}`).end((err, res) => {
+            console.log(res.body);
+            res.should.have.status(401);
+            expect(res.body).to.be.an("object");
+            expect(res.body).to.have.property("message");
+            done();
+        });
     });
 
-    it("should not delete report at DELET /authority/reports/reportId is not authority admin", function (done) {
-        agent
-            .delete(`/authority/testauthority/reports/${reportId}`)
-            .set("content-type", "application/json")
-            .send()
-            .then(function (res) {
-                res.should.have.status(401);
-                expect(res.body).to.be.an("object");
-                expect(res.body).to.have.property("error");
-                done();
-            })
-            .catch((err) => {
-                done(err);
-            });
+    it("should delete report at DELETE /reports/:reportId", function (done) {
+        agent.delete(`/reports/${reportId}`).end((err, res) => {
+            console.log(res.body);
+            res.should.have.status(200);
+            expect(res.body).to.be.an("object");
+            expect(res.body).to.have.property("message");
+            done();
+        });
     });
 
     after(async function () {
