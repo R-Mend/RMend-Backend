a7ba79c314b855dc232150d5935298cdddae6947
TannerYork <tannerwyork@gmail.com>
2021-07-24 13:36:34 -0500

    Add get/update authority user routes

--- controllers/authorities.js
+++ controllers/authorities.js
@@ -77,9 +77,90 @@ module.exports = function (app) {
             return res.status(400).send({ message: "Error occured while deleting request." });
         }
 
-        console.log("SENDING SUCCESS RESPONSE");
         // Send a successfull response
         return res.status(200).send({ message: "User has been accepted into the authority." });
+    });
+
+    app.get("/authority/:userId", async (req, res) => {
+        // Get authority's name from url params
+        const { userId } = req.params;
+
+        // Veirfy user is logged in
+        const user = req.user;
+        if (!user) {
+            return res.status(401).send({ message: "You need to be logged in to access this route." });
+        }
+
+        // Verify that user is a part of an authority and is an admin
+        const authority = user.authority;
+        if (!authority || user.access_level != "admin") {
+            // Send a failed response if not found
+            return res.status(401).send({ message: "Access denied. You do not enough access to this authority" });
+        }
+
+        // Verify requested user exists
+        const reqUser = await User.findById(userId);
+        if (!reqUser) {
+            return res.status(400).send({ message: "Requested user does not exist." });
+        }
+
+        // Verify requested user is a member of users authority
+        if (reqUser.authority != String(user.authority._id)) {
+            return res.status(400).send({ message: "Requested user is not a member of your organization." });
+        }
+
+        // Return successful response with requested user
+        return res.status(200).send({ user: reqUser });
+    });
+
+    app.put("/authority/:userId", async (req, res) => {
+        // Get authority's name from url params
+        const { userId } = req.params;
+
+        // Veirfy user is logged in
+        const user = req.user;
+        if (!user) {
+            return res.status(401).send({ message: "You need to be logged in to access this route." });
+        }
+
+        // Verify that user is a part of an authority and is an admin
+        if (!user.authority || user.access_level != "admin") {
+            // Send a failed response if not found
+            return res.status(401).send({ message: "Access denied. You do not enough access to this authority" });
+        }
+
+        // Verify request data is not missing access_level
+        const { access_level } = req.body;
+        if (!access_level) {
+            return res.status(400).send({ message: "Missing required parameter access_level." });
+        }
+
+        // Verify access_level is valid option
+        if (["admin", "employee", "user"].indexOf(access_level) == -1) {
+            return res.status(400).send({ message: "Access level needs to be either admin, employee, or user" });
+        }
+
+        // Verify requested user exists
+        const reqUser = await User.findById(userId);
+        if (!reqUser) {
+            return res.status(400).send({ message: "Requested user does not exist." });
+        }
+
+        // Verify requested user is a member of the users authority
+        if (reqUser.authority != String(user.authority._id)) {
+            return res.status(400).send({ message: "Requested user is not a member of your authority." });
+        }
+
+        // Update users access_level
+        const updateRes = await User.updateOne({ _id: reqUser._id }, { access_level: access_level });
+
+        // Verify update was a success
+        if (updateRes.nModified == 0) {
+            return res.status(400).send({ message: "Error occured while update users access level." });
+        }
+
+        // Send a successful response
+        return res.status(200).send({ message: "Successfully update user's acces level." });
     });
 
     app.get("/authority/reports", async (req, res) => {
