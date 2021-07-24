f384f0d7f8d17cb964892458a33c6974238ad400
Tanner W. York <tyork@zeta-apps.com>
2021-07-24 17:43:19 -0500

    Update tests/routes for authority reports

--- models/Report.js
+++ models/Report.js
@@ -1,11 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const ReportSchema = new Schema({
-  title: { type: String, required: true },
-  details: { type: String, required: true},
-  author: { type: Schema.Types.ObjectId, ref: "User", required: true},
-  organization: { type: Schema.Types.ObjectId, ref: "Organization", required: true }
-}, {timestamps: true})
+const ReportSchema = new Schema(
+    {
+        title: { type: String, required: true },
+        details: { type: String, required: true },
+        author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+        authority: { type: Schema.Types.ObjectId, ref: "Authority", required: true },
+    },
+    { timestamps: true }
+);
 
 module.exports = mongoose.model("Report", ReportSchema);
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const dotenv = require('dotenv');
-const express = require('express');
-const expressValidator = require('express-validator');
-var cookieParser = require('cookie-parser');
-const jwt = require('jsonwebtoken');
-const User = require('./models/User.js');
+const dotenv = require("dotenv");
+const express = require("express");
+const expressValidator = require("express-validator");
+var cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+const User = require("./models/User.js");
 dotenv.config();
 
 // Set Database
-require('./data/report-db');
+require("./data/report-db");
 
 // Create app
 const app = express();
@@ -22,31 +22,30 @@ app.use(cookieParser());
 
 // Auth Checker
 var checkAuth = async (req, res, next) => {
-  console.log("Checking authentication");
-  if (typeof req.cookies.nToken === "undefined" || req.cookies.nToken === null) {
-    req.user = null;
-  } else {
-    var token = req.cookies.nToken;
-    var decodedToken = jwt.decode(token, { complete: true }) || {};
-    var user = decodedToken.payload;
+    if (typeof req.cookies.nToken === "undefined" || req.cookies.nToken === null) {
+        req.user = null;
+    } else {
+        var token = req.cookies.nToken;
+        var decodedToken = jwt.decode(token, { complete: true }) || {};
+        var user = decodedToken.payload;
 
-    current_user = await User.findById(user._id).populate('authority');
-    req.user = current_user
-  }
-  next();
+        current_user = await User.findById(user._id).populate("authority");
+        req.user = current_user;
+    }
+    next();
 };
 app.use(checkAuth);
 
 // Setup Controllers
-require('./controllers/auth.js')(app);
-require('./controllers/reports.js')(app);
-require('./controllers/authorities.js')(app);
+require("./controllers/auth.js")(app);
+require("./controllers/reports.js")(app);
+require("./controllers/authorities.js")(app);
 
 // Listen to Port
-if(!module.parent){
-  app.listen(process.env.PORT, () => {
-    console.log(`Report Manager listening on http://localhost:${[process.env.PORT]}`)
-  });
+if (!module.parent) {
+    app.listen(process.env.PORT, () => {
+        console.log(`Report Manager listening on http://localhost:${[process.env.PORT]}`);
+    });
 }
 
 module.exports = app;
